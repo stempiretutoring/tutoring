592c463b3810937fa70b42225fbd4e4c177c373e
Jeevan Shah <jedimaster3284@gmail.com>
2023-12-29 18:54:44 -0500

    fix breaking chanegs

--- src/app/api/auth/tutors/route.ts
+++ src/app/api/auth/tutors/route.ts
@@ -17,4 +17,4 @@ export async function GET(request: NextRequest) {
   }
 }
 
-export const runtime = 'experimental-edge'
+export const runtime = 'edge'
--- src/app/api/tutors/route.ts
+++ src/app/api/tutors/route.ts
@@ -73,4 +73,4 @@ export async function POST(request: NextRequest) {
   }
 }
 
-export const runtime = 'experimental-edge'
+export const runtime = 'edge'
