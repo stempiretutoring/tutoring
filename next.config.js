cc2f50cdbbe28ffd0078cedd30bb771901ba1324
Jeevan Shah <jedimaster3284@gmail.com>
2023-12-29 18:47:50 -0500

    fix breaking changes (please)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+  experimental: {
+    runtime: 'experimental-edge',
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+}
 module.exports = nextConfig
