a9bcec7c5d937a1f72ba6b4b4808a53fd6951de4
Jeevan Shah <jedimaster3284@gmail.com>
2024-01-04 22:52:33 -0500

    add lib

---
+++ src/app/user/admin/lib/data.ts
@@ -0,0 +1,22 @@
+export const columns = [
+  {
+    key: "name",
+    label: "NAME",
+  },
+  {
+    key: "email",
+    label: "EMAIL",
+  },
+  {
+    key: "subjects",
+    label: "SUBJECTS",
+  },
+  {
+    key: "active",
+    label: "STATUS",
+  },
+  {
+    key: "actions",
+    label: "ACTIONS",
+  },
+];
