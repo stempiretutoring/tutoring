82ec5be487579551dbd0322c59ea07a36f76f0d9
Jeevan Shah <jedimaster3284@gmail.com>
2024-02-10 15:54:28 -0500

    overflow menu for subjects

---
+++ src/app/tutors/components/subjects.tsx
@@ -0,0 +1,53 @@
+"use client";
+import React from "react";
+import {
+  Chip,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+} from "@nextui-org/react";
+import { PiCursorClickFill } from "react-icons/pi";
+
+interface subjectProps {
+  subjects: string[];
+}
+
+export default function Subjects({ subjects }: subjectProps) {
+  if (subjects.length <= 4) {
+    return (
+      <div>
+        {subjects.map((subject, idx) => (
+          <Chip key={idx} className="w-full m-1 capitalize">
+            {subject}
+          </Chip>
+        ))}
+      </div>
+    );
+  } else if (subjects.length > 4) {
+    const firstSubs: string[] = subjects.slice(0, 3);
+    const lastSubs: string[] = subjects.slice(4);
+    return (
+      <div>
+        {firstSubs.map((subject, idx) => (
+          <Chip key={idx} className="w-full m-1 capitalize">
+            {subject}
+          </Chip>
+        ))}
+        <Popover placement="bottom">
+          <PopoverTrigger>
+            <Chip variant="bordered" endContent={<PiCursorClickFill />}>More</Chip>
+          </PopoverTrigger>
+          <PopoverContent>
+            <div>
+              {lastSubs.map((subject, idx) => (
+                <Chip key={idx} className="w-full m-1 capitalize">
+                  {subject}
+                </Chip>
+              ))}
+            </div>
+          </PopoverContent>
+        </Popover>
+      </div>
+    );
+  }
+}
