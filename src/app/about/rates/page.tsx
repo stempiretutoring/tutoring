c865da55fdb599c147396d2625fe9656ebf08a85
Jeevan Shah <jedimaster3284@gmail.com>
2024-02-13 22:07:15 -0500

    update mobile view for rates table

--- src/app/about/rates/page.tsx
+++ src/app/about/rates/page.tsx
@@ -15,12 +15,12 @@ import {
 export default function App() {
   return (
     <div className="h-dvh">
-      <h1 className="mx-auto my-4 w-2/3 text-xl">
+      <h1 className="mx-auto my-4 w-10/12 md:w-2/3 text-xl">
         STEMpire focuses on being very affordable while still offering the
         highest quality of education
       </h1>
       <Divider />
-      <Table isStriped className="mx-auto mt-3 w-1/2" aria-label="Rates table">
+      <Table isStriped className="mx-auto mt-3 w-11/12 md:w-1/2" aria-label="Rates table">
         <TableHeader>
           <TableColumn># of Students in session</TableColumn>
           <TableColumn>Cost (per person)</TableColumn>
@@ -44,16 +44,16 @@ export default function App() {
           </TableRow>
         </TableBody>
       </Table>
-      <p className="text-sm italic md:hidden mx-auto w-1/2 pt-2 pl-1">
+      <p className="text-sm text-slate-900 italic md:hidden mx-auto w-3/4 md:w-1/2 pt-2 pl-1">
         * On devices with smaller screens you may have to scroll to view the
         entire table
       </p>
-      <p className="mx-auto w-1/2">
-        *Note that prices are subject to processing fees
+      <p className="mx-auto text-slate-700 w-3/4 md:w-1/2">
+        * Note that prices are subject to processing fees
       </p>
       <div className="mx-auto w-1/2">
         <Link href="/book">
-          <Button className="mt-3" color="success">
+          <Button className="mt-5 md:mt-3" color="success">
             Book your session now!
           </Button>
         </Link>
